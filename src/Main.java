e24e099e63ed7ce624e41c3b851fbd34389c34f0
didilero <didileroVA1999@hotmail.fr>
2018-09-24 09:11:54 +0200

    new test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Bjr madame
 		PokedexInterface maFenetre = new PokedexInterface();
 	}
 
